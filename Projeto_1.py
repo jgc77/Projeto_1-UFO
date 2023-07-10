9025566796d31cae84101385db2f34827d5a21d1
João Gustavo Cavalcanti Beltrão da Silva <54086688+jgc77@users.noreply.github.com>
2023-07-10 14:00:03 -0300

    Adicionando dados

--- Projeto_1.py
+++ Projeto_1.py
@@ -7,3 +7,14 @@ UFO Sightings
 
 """
 
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#Carregar dataset
+dt_ufo = pd.read_csv('C:\\Users\\joaog\\Documents\\UPE\\LPAA\\Projeto\\Datasets\\scrubbed.csv')
+dt_ufo.info
+
+# Convertendo as datas para o modo datetime
+dt_ufo['datetime'] = pd.to_datetime(dt_ufo['datetime'], errors='coerce')
+dt_ufo['date posted'] = pd.to_datetime(dt_ufo['date posted'], errors='coerce')
