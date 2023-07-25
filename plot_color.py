89f6aabd6e4ae28575d7921be07b779663fbf91d
João Gustavo Cavalcanti Beltrão da Silva <joaogustavocavalcante@gmail.com>
2023-07-25 00:05:44 -0300

    Organizando código

--- plot_color.py
+++ plot_color.py
@@ -2,166 +2,82 @@
 """
 Created on Tue Jul 18 09:34:08 2023
 
-@author: joaog
+Autor: joaog
 """
 import matplotlib.pyplot as plt
-import matplotlib.cm as cm
 import numpy as np
-import pandas as pd
 import geopandas as gpd
-#from matplotlib.collections import LineCollection
-#from matplotlib.colors import Normalize
-
-
 
 
 class Plot_color:
     
-    #Função para plotar em degrade
-    def c_bar(data, colormap, title, xlabel, ylabel, rt, grid, jump):
-        
-        #Definindo tamanho e dpi do gráfico
+    # Função para plotar gráfico de barras com degradê de cores
+    def c_bar(data, colormap, title, xlabel, ylabel, rotation, grid, x_tick_jump):
         plt.figure(figsize=(10, 6), dpi=300)
-        
-        # Normalização dos valores para [0, 1]
         norm_valores = (data - np.min(data)) / (np.max(data) - np.min(data))
-
-        # Criação do gráfico de barras com degradê de cores
         plt.bar(data.index, data.values, color=colormap(norm_valores))
-        
-        #Legendas
-        plt.title(title)
-        plt.xlabel(xlabel)
-        plt.ylabel(ylabel)
+        plt.title(title,fontsize=12)
+        plt.xlabel(xlabel,fontsize=12)
+        plt.ylabel(ylabel,fontsize=15)
         plt.grid(grid)
-        
-        # Adicionando legenda de cores (colorbar)
         sm = plt.cm.ScalarMappable(cmap=colormap, norm=plt.Normalize(vmin=np.min(data), vmax=np.max(data)))
-        sm._A = []  
+        sm._A = []
         plt.colorbar(sm, ax=plt.gca(), pad=0.05)
-        
-        #Rotação
-        plt.xticks(rotation=rt)
-        plt.xticks(data.index[::jump], data.index[::jump])
-        
-    '''
-    #Função para plotar em degrade
-    def c_line(data):
-        
-        x = np.arange(len(data))  # Índices numéricos para representar as horas
-        y = data["duration (seconds)"].values  # Valores da quantidade para cada hora
+        plt.xticks(rotation=rotation)
+        plt.xticks(data.index[::x_tick_jump], data.index[::x_tick_jump])
 
-        # Calcula o primeiro derivado dos dados y
-        dydx = np.gradient(y)
-
-        # Criação dos pontos e das segmentos usando os arrays x e y
-        points = np.array([x, y]).T.reshape(-1, 1, 2)
-        segments = np.concatenate([points[:-1], points[1:]], axis=1)
-
-        fig, ax = plt.subplots(figsize=(10, 6), dpi=300)
-        
-        # Cria uma normalização para mapear os pontos para as cores do mapa de cores
-        norm = Normalize(dydx.min(), dydx.max())
-        lc = LineCollection(segments, cmap='viridis', norm=norm)
-        lc.set_array(dydx)
-        lc.set_linewidth(2)
-        line = ax.add_collection(lc)
-        fig.colorbar(line, ax=ax)
-        plt.grid(True)
-        
-
-        ax.set_xlim(x.min(), x.max())
-        ax.set_ylim(y.min() - 38, y.max() + 38)  # Ajuste o limite vertical conforme necessário
-        plt.show()
-    '''
-    
-    #Função para plotar em barra
-    def bar(data, cor, title ,xlabel, ylabel, rt, grid):
-        
-        #Definindo tamanho e dpi do gráfico
+    # Função para plotar gráfico de barras
+    def bar(data, cor, title ,xlabel, ylabel, rotation, grid):
         plt.figure(figsize=(10, 6), dpi=300)
-        
-        #Criação do gráfico de barras
         plt.barh(data.index, data.values, color=cor)
-        
-        #Legendas
-        plt.xlabel(xlabel)
-        plt.ylabel(ylabel)
-        plt.title(title)
+        plt.xlabel(xlabel, fontsize=12)
+        plt.ylabel(ylabel, fontsize=12)
+        plt.title(title, fontsize=15)
         plt.grid(grid)
+        plt.xticks(rotation=rotation)
 
-        #Rotação
-        plt.xticks(rotation=rt)
-        
-    #Função para plotar em linha
-    def linha(data, cor, title ,xlabel, ylabel, rt, grid):
-        
-        #Definindo tamanho e dpi do gráfico
+    # Função para plotar gráfico de linhas
+    def linha(data, cor, title ,xlabel, ylabel, rotation, grid):
         plt.figure(figsize=(10, 6), dpi=300)
-        
-        #Criação do gráfico de barras
         plt.plot(data.index, data.values, color=cor)
-        
-        #Legendas
-        plt.xlabel(xlabel)
-        plt.ylabel(ylabel)
-        plt.title(title)
+        plt.xlabel(xlabel,fontsize=12)
+        plt.ylabel(ylabel,fontsize=12)
+        plt.title(title,fontsize=15)
         plt.grid(grid)
-        
-        #Rotação
-        plt.xticks(rotation=rt)
-        
-     
-   #Função para plotar mapa mundi    
+        plt.xticks(rotation=rotation)
+
+    # Função para plotar mapa mundi
     def mapa(data,eixo1, eixo2):
-        
-        #importar mapa mundi
         world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
-        
-        #plotar mapa mundi
-        ax = world.plot(figsize=(40, 35),color='#DCDCDC', edgecolor='k')
+        ax = world.plot(figsize=(40, 35), color='#DCDCDC', edgecolor='k')
         ax.set_facecolor('#B0C4DE')
-               
-        #legendas
-        ax.set_xticks([])
-        ax.set_yticks([])
-        
-        #plotar marcadores
-        data.plot.scatter(x=eixo1, y=eixo2, ax=ax, color='y', marker='.', s=30, edgecolor='b', linewidth=0.4)
-    
-    #Função para plotar mapa dos Estados Unidos 
+        plt.xlabel('Longitude', fontsize = 30)
+        plt.ylabel('Latitude', fontsize = 30)
+        plt.title('Mapa Mundi', fontsize = 40)
+        data.plot.scatter(x=eixo1, y=eixo2, ax=ax, color='y', marker='.', s=35, edgecolor='r', linewidth=0.4)
+
+    # Função para plotar mapa dos Estados Unidos
     def mapa_us(data, eixo1, eixo2, xlabel, ylabel):
-        
-        #Filtar Estados Unidos de data
         data_us = data[data['country'] == 'us']
-        
-        #Importar mapa dos Estados Unidos
         usa_states = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
         usa_states = usa_states[usa_states['name'].isin(['United States of America'])]
-
-        #plotar mapa dos Estados Unidos
-        ax = usa_states.plot(figsize=(25, 20),color='#DCDCDC', edgecolor='k')
+        ax = usa_states.plot(figsize=(25, 20), color='#DCDCDC', edgecolor='k')
         ax.set_facecolor('#B0C4DE')
-
-        #legendas
-        ax.set_xticks([])
-        ax.set_yticks([])
-        
-        #plotar marcadores
+        plt.xlabel('', fontsize = 20)
+        plt.ylabel('', fontsize = 20)
+        plt.title('EUA', fontsize = 40)
         data_us.plot.scatter(x=eixo1, y=eixo2, ax=ax, color='red', marker='.', s=20, edgecolor='b', linewidth=0.4)
-    
-    #Plotar grafico em pizza com distribuição de ocorrencias por continente 
-    def c_ocorren(data, colors, title, labels, explode):    
-        
-        plt.subplots(figsize=(18, 10))
-        
-        plt.pie(data, explode=explode, colors= colors, autopct='%1.1f%%', textprops={'fontsize': 22}, labels= None, shadow=None)
-        
-        plt.legend(labels=labels, loc='lower right')
-        plt.title(title, fontsize = 20)
+
+    # Função para plotar gráfico de pizza
+    def c_ocorren(data, colors, title, labels, explode):
+        plt.subplots(figsize=(22, 14))
+        plt.pie(data, explode=explode, colors= colors, autopct='%1.1f%%', textprops={'fontsize': 22}, pctdistance=0.83, labels= None, shadow=None)
+        plt.legend(labels=labels, loc='lower right', fontsize = 15)
+        plt.title(title, fontsize = 29)
         plt.axis('equal')
         plt.xticks(rotation=45, fontsize=15)
 
+    # Dicionario para siglas de estado do USA
     def dici_state(data):
         estados = {
             'ca': 'California',
